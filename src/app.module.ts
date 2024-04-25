5f5b5792da33a744538ce9393ce027e8ac944fd5
Roman Polunin <ramstex88@gmail.com>
2024-04-25 18:45:33 +0400

    feat(structure): Nest config updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-    imports: [],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.join(__dirname, '..', 'static'),
+            serveRoot: '/',
+            exclude: ['/api*'],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { createTestingApp } from './utils/create-testing-app';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
     beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
-
-        app = moduleFixture.createNestApplication();
+        app = await createTestingApp();
         await app.init();
     });
 
     it('/ (GET)', () => {
         return request(app.getHttpServer())
-            .get('/')
+            .get('/api')
             .expect(200)
             .expect('Hello World!');
     });
